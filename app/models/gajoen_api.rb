13a483a3f049a489b0b932ea38932d0404e7aa9c
ogadra <61941819+ogadra@users.noreply.github.com>
2021-08-19 15:27:20 +0900

    gajoenを叩くAPIの実装

---
+++ app/models/gajoen_api.rb
@@ -0,0 +1,29 @@
+require 'net/https'
+require 'uri'
+require 'securerandom'
+
+class GajoenApi
+  def self.create_tickets(brand_id, item_id)
+    request_code = SecureRandom.urlsafe_base64(30)
+    query={
+      'item_id'=>item_id,
+      'request_code'=>request_code
+    }
+    url = ENV['GAJOEN_URI']+ "/brands/#{brand_id}/tickets/"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req['Authorization'] = ENV['GAJOEN_HEADER']
+    req['X-Giftee'] = 1
+    req.set_form_data(query)
+    res = http.request(req)
+    case res
+    when Net::HTTPSuccess
+      return JSON.parse(res.body)
+    else
+      return nil
+    end
+  end
+end
