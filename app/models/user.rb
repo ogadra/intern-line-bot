42e0024df987e506f635d8ad776280ad37d2726a
ogadra <61941819+ogadra@users.noreply.github.com>
2021-08-19 11:58:32 +0900

    論理削除ロジックをmodelに実装 ユーザーの削除・追加は他のcontrollerから触る可能性が高いため、modelに書き直しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   validates :line_user_id, presence: true, uniqueness: true
   validates :friend_registration_datetime, presence: true
+
+  def register(line_user_id, datetime)
+    existed_user = User.find_or_create_by(line_user_id: line_user_id) do |user|
+      user.friend_registration_datetime = datetime
+    end
+    existed_user.update(is_blocked: false)
+
+  end
+
+  def archive(user_id)
+    @user = User.find_by(line_user_id: user_id)
+    @user.update(is_blocked: true )
+  end
 end
